d5a6f21ede16bab22f592858a5aeeb781d148822
wiktortr <wiktortr9@gmail.com>
2018-06-12 20:43:19 +0200

    Add Validation Of Move

--- renderer.js
+++ renderer.js
@@ -1,5 +1,7 @@
+let game;
+let currentPlayer = 0;
 
-let chessboard = [
+const chessboard = [
     [-1, -2, -3, -4, -5, -3, -2, -1],
     [-6, -6, -6, -6, -6, -6, -6, -6],
     [ 0,  0,  0,  0,  0,  0,  0,  0],
@@ -8,6 +10,116 @@ let chessboard = [
     [ 0,  0,  0,  0,  0,  0,  0,  0],
     [ 6,  6,  6,  6,  6,  6,  6,  6],
     [ 1,  2,  3,  4,  5,  3,  2,  1]
+];
+
+const moves = [
+    {
+        name: "pionek",
+        id: 6,
+        start: {
+            x: 2,
+            y: 2,
+        },
+        move: [
+            [0,0,0,0,0],
+            [0,0,2,0,0],
+            [0,0,0,0,0],
+            [0,0,0,0,0],
+            [0,0,0,0,0],
+        ]
+    },
+    {
+        name: "wieza",
+        id: 1,
+        start: {
+            x: 8,
+            y: 8,
+        },
+        move: [
+            [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+            [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+        ]
+    },
+    {
+        name: "kunik",
+        id: 2,
+        start: {
+            x: 0,
+            y: 0,
+        },
+        move: [
+
+        ]
+    },
+    {
+        name: "goniec",
+        id: 3,
+        start: {
+            x: 8,
+            y: 8,
+        },
+        move: [
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+            [0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
+            [0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0],
+            [0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0],
+            [0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0],
+            [0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0],
+            [0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0],
+            [0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0],
+            [0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0],
+            [0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0],
+            [0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0],
+            [0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0],
+            [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        ]
+    },
+    {
+        name: "hetman",
+        id: 4,
+        start: {
+            x: 8,
+            y: 8,
+        },
+        move: [
+            [1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1],
+            [0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0],
+            [0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0],
+            [0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0],
+            [0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0],
+            [0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0],
+            [0,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0],
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+            [0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,1,0,1,0,1,0,0,0,0,0,0],
+            [0,0,0,0,0,1,0,0,1,0,0,1,0,0,0,0,0],
+            [0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0],
+            [0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0],
+            [0,0,1,0,0,0,0,0,1,0,0,0,0,0,1,0,0],
+            [0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0],
+            [1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1],
+        ]
+    },
 ];
 
 function generateBoard(board, view, sets){
@@ -41,19 +153,123 @@ function generateBoard(board, view, sets){
     }
 }
 
+async function makeNewGame(){
+    return new Promise((resolve, reject) => {
+        let clone_board = new Array();
+        for (let i = 0; i < chessboard.length; i++) {
+            let arr = new Array();
+            for (let j = 0; j < chessboard[i].length; j++) {
+                arr.push(chessboard[i][j]);              
+            }   
+            clone_board.push(arr);
+        }
+        resolve({
+            board: clone_board,
+            players: [
+                {
+                    side: "black"
+                },
+                {
+                    side: "white"
+                }
+            ]
+        })
+    });
+}
+
+function makeMove(current_player, game, x1, y1, x2, y2){
+    return new Promise((resolve, reject) => {
+        let pawn = game.board[y1][x1];
+        let pawn_id = Math.abs(pawn);
+        let move = null;
+        for (let i = 0; i < moves.length; i++) {
+            if(moves[i].id == pawn_id){
+                move = moves[i];
+                break;
+            }
+        }
+        if(move !== null){
+            let x = Math.abs(x1 - x2);
+            let y = Math.abs(y1 - y2);
+            let mx = move.start.x - x;
+            let my = move.start.y - y;
+            
+            let vx = 0;
+            let vy = 0;
+            if(x1 > x2){ vx = -1; }
+            if(x1 < x2){ vx =  1; }
+            if(y1 > y2){ vy = -1; }
+            if(y1 < y2){ vy =  1; }
+            
+            let block = false;
+            let cx = x1;
+            let cy = y1;
+            while (!block) {
+                cx += vx;
+                cy += vy;
+                if(game.board[cy][cx] != 0){
+                    block = true;
+                }
+                if(cx == x2 && cy == y2){
+                    break;
+                }
+            }
+            
+            if(block){
+                reject("You are blocked");
+            }
+
+            try {
+                let state = move.move[my][mx];
+
+                switch (state) {
+
+                    case 1:{
+                        if(game.board[y2][x2] != 0 && ((pawn < 0 && game.board[y2][x2] < 0) || (pawn > 0 && game.board[y2][x2] > 0))){
+                            reject("You can't beat your own pawn!");
+                        }
+                        else{
+                            resolve("OK");
+                        }
+                        break;
+                    }
+
+                    case 2:{
+                        if(game.board[y2][x2] == 0){
+                            resolve("OK");
+                        }
+                        else{
+                            reject("not ok");
+                        }
+                    }
+
+                    default:{
+                        reject("Undefined command");
+                        break;
+                    }
+                }
+            }
+            catch (error) {
+                reject("This move is incorrent");
+            }
+        }
+        else{
+            reject("Can't find this move in db");
+        }
+    });
+}
+
 function moveEvent() {
     let drag = false;
-    let startX, startY, startMX, startMY;
+    let x1, y1;
     let parent;
     let pawn;
 
     $(document).on("mousedown", ".chessboard-pawn", function(e){
         pawn = $(this);
         drag = true;
-        startX = pawn.offset().left;
-        startY = pawn.offset().top;
-        startMX = e.pageX;
-        startMY = e.pageY;
+        x1 = pawn.parent().index();
+        y1 = pawn.parent().parent().index();
         pawn.css({
             width: pawn.width() + "px",
             height: pawn.height() + "px",
@@ -80,21 +296,35 @@ function moveEvent() {
             for (let i = 0; i < $(".chessboard-field").length; i++) {
                 let f = $(".chessboard-field").eq(i);
                 let o = f.offset();
-                if(o.left < e.pageX && e.pageX < (o.left + f.width()) && o.top < e.pageY && e.pageY < (o.top + f.height()) && f.children().length == 0){
+                if(o.left < e.pageX && e.pageX < (o.left + f.width()) && o.top < e.pageY && e.pageY < (o.top + f.height())){
                     field = f;
                     break;
                 }
             }
 
             if(field !== null){
-                let o = $(pawn).clone();
-                o.css({
-                    left: "0px",
-                    top: "0px",
-                    position: "static",
-                });
-                $(field).append(o);
-                $(pawn).remove();
+                let x2 = field.index();
+                let y2 = field.parent().index();
+                makeMove(currentPlayer, game, x1, y1, x2, y2).then((e) => {
+                    let o = $(pawn).clone();
+                    o.css({
+                        left: "0px",
+                        top: "0px",
+                        position: "static",
+                    });
+                    $(field).html(o);
+                    $(pawn).remove();
+                    let p_id = game.board[y1][x1];
+                    game.board[y1][x1] = 0;
+                    game.board[y2][x2] = p_id;
+                }).catch((e) => {
+                    $(pawn).css({
+                        left: "0px",
+                        top: "0px",
+                        position: "static",
+                    });    
+                    console.log(e);
+                })
             }
             else{
                 $(pawn).css({
@@ -109,22 +339,21 @@ function moveEvent() {
 
 $(document).ready(function (e) {
 
-    setInterval(function () {
-        var haj = $("#test").css("height");
-        document.getElementById("sidebar").style.height = haj;
-    }, 1500);
-
     generateBoard($("#board0"), "black", chessboard);
     moveEvent();
 
-    calcMove(chessboard, "black").then((e) => {
-        console.log(e);
-    });
-
     $(window).on("resize", function(){
+        let h = $("#test").css("height");
+        $("#sidebar").css("height", h + "px");
         for (let i = 0; i < $(".chessboard").length; i++) {
             $(".chessboard").eq(i).height($(".chessboard").eq(i).width());
         }
+    });
+
+    makeNewGame().then((g) => {
+        game = g;
+        generateBoard($("#board0"), "black", game.board);
+        console.log("Create new game");
     });
 
 });
